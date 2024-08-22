d0140960fd4505f66fb25457d2c035293d2468fe
Matt Ostrom-Hall <matt.ostrom-hall@reallysimplesystems.com>
2024-08-22 13:45:29 +0100

    updated chrome driver command based on main repo

--- src/Console/ChromeDriverCommand.php
+++ src/Console/ChromeDriverCommand.php
@@ -93,7 +93,7 @@ class ChromeDriverCommand extends Command
             if ($all || ($os === $currentOS)) {
                 $archive = $this->download($version, $os);
 
-                $binary = $this->extract($version, $archive);
+                $binary = $this->extract($archive);
 
                 $this->rename($binary, $os);
             }
@@ -217,35 +217,37 @@ class ChromeDriverCommand extends Command
     /**
      * Extract the ChromeDriver binary from the archive and delete the archive.
      *
-     * @param  string  $version
-     * @param  string  $archive
+     * @param string $archive
      * @return string
+     * @throws Exception
      */
-    protected function extract($version, $archive)
+    protected function extract($archive)
     {
         $zip = new ZipArchive;
 
         $zip->open($archive);
 
-        $zip->extractTo($this->directory);
+        $binary = null;
 
-        switch (true) {
-            case version_compare($version, '115.0', '<'):
-                $index = 0;
+        for ($fileIndex = 0; $fileIndex < $zip->numFiles; $fileIndex++) {
+            $filename = $zip->getNameIndex($fileIndex);
+
+            if (Str::startsWith(basename($filename), 'chromedriver')) {
+                $binary = $filename;
+
+                $zip->extractTo($this->directory, $binary);
+
                 break;
-            case version_compare($version, '127.0', '<'):
-                $index = 1;
-                break;
-            default:
-                $index = 2;
-                break;
+            }
         }
-
-        $binary = $zip->getNameIndex($index);
 
         $zip->close();
 
         unlink($archive);
+
+        if (! $binary) {
+            throw new Exception('Could not extract the ChromeDriver binary.');
+        }
 
         return $binary;
     }
